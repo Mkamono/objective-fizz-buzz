e6e6e51f70825d86f07fbeead622b7ef53c7e4ca
Mkamono <chengyaye31@gmail.com>
2024-06-19 01:02:23 +0000

    init

---
+++ fizzbuzz/fizzbuzz.go
@@ -0,0 +1,14 @@
+package fizzbuzz
+
+func FizzBuzz(n int) string {
+	switch {
+	case n%15 == 0:
+		return "FizzBuzz"
+	case n%3 == 0:
+		return "Fizz"
+	case n%5 == 0:
+		return "Buzz"
+	default:
+		return ""
+	}
+}
